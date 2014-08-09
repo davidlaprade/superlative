ee8574fcceac5554ece9b2e0701665483e8955ee
davidlaprade <david.laprade@gmail.com>
2014-08-09 09:11:00 -0400

    error messages work

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ class User < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true
+  validates_uniqueness_of :email, message: 'must be unique'
   validates :photo, presence: true
   validates :track, presence: true
 
