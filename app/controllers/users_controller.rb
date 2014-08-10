bf04f442121b4cab3b8b9b77af3fbf49257e9c88
davidlaprade <david.laprade@gmail.com>
2014-08-10 11:00:57 -0400

    added users show route, action, view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,13 @@ class UsersController < ApplicationController
 	def index
 		@users = User.all
 	end
+
+	def show
+		@user = User.find_by_id(params[:user_id])
+	end
+
+	private
+	def user_params
+		params.require(:user).permit(:user_id)
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'users#index'
 
+  match 'users/:id', :to => 'users#show', :as => :user, :via => :get
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
