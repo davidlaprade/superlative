69e6bfd431252439199af2890928ade935164b62
davidlaprade <david.laprade@gmail.com>
2014-08-10 14:49:00 -0400

    added submit form to title view

--- app/controllers/titles_controller.rb
+++ app/controllers/titles_controller.rb
@@ -5,6 +5,17 @@ class TitlesController < ApplicationController
 		@title = @user.titles.new
 	end
 
+	def create
+		@user = User.find_by_id(params[:user_id])
+		@title = @user.titles.new(title_params)
+		@title.created_by = current_user.id
+
+		if @title.save
+			render user_path(@user.id)
+		end
+	end
+
+
 	private
 	def title_params
 		params.require(:title).permit(:content)
