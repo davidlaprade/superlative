04189afb7ea9240513f8e3dfb8affbfb4b559c42
davidlaprade <david.laprade@gmail.com>
2014-08-10 10:25:01 -0400

    tried to add one-favorite-per-user-per-user validation

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -3,4 +3,14 @@ class Favorite < ActiveRecord::Base
 belongs_to :user
 belongs_to :title
 
+
+# A user should not be able to favorite multiple titles for a given user
+# 	See http://stackoverflow.com/questions/1486020/validates-uniqueness-of-scope-on-other-table
+# 	See http://stackoverflow.com/questions/9033797/how-to-specify-conditions-on-joined-tables-in-rails
+# validates_each :user_id do |record, attr, value|
+#   if Favorite.joins(:title).where(:titles => {:user_id => value}).present?
+#     record.errors.add attr, 'Can only favorite one superlative per person!'
+#   end
+# end
+
 end
--- app/models/title.rb
+++ app/models/title.rb
@@ -3,4 +3,6 @@ class Title < ActiveRecord::Base
 has_many :favorites
 has_many :votes, through: :favorites, source: :user
 
+validates_uniqueness_of :content
+
 end
