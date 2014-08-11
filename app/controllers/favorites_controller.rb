56688394374427660175c1f1aeb4e251216abc5a
davidlaprade <david.laprade@gmail.com>
2014-08-10 22:30:28 -0400

    added unfavorite action, added view logic governing appearance of favorite/unfavorite buttons

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -14,6 +14,16 @@ class FavoritesController < ApplicationController
 	end
 
 	def unfavorite
+		@title = Title.find_by_id(params[:id])
+		@favorites = Favorite.where(user_id: current_user.id).where(title_id: @title.id)
+		@favorites.destroy_all
+
+		if @favorite.destroy_all
+			redirect_to user_path(params[:user_id])
+		else
+			redirect_to root_path
+		end
+
 	end
 
 end
