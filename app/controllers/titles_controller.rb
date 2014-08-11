24e2ce275a94f6ec2fe2b31ce3c32cdf2d1cce92
davidlaprade <david.laprade@gmail.com>
2014-08-10 20:49:50 -0400

    submit form working, parameters saving, fixed views

--- app/controllers/titles_controller.rb
+++ app/controllers/titles_controller.rb
@@ -7,11 +7,13 @@ class TitlesController < ApplicationController
 
 	def create
 		@user = User.find_by_id(params[:user_id])
-		@title = @user.titles.new(title_params)
+		@title = @user.titles.create(title_params)
 		@title.created_by = current_user.id
 
 		if @title.save
-			render user_path(@user.id)
+			redirect_to root_path
+		else
+			render 'new'
 		end
 	end
 
