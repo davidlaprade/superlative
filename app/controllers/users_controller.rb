d7cfe0d4472418bb5466294b33ce0707f59caf29
davidlaprade <david.laprade@gmail.com>
2014-08-14 08:56:25 -0400

    guest votes and suggested superlatives delete on sign out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,16 +6,18 @@ class UsersController < ApplicationController
 
 	def index
 		@users = User.all
+		if !(user_signed_in? && current_user.role == "guest")
+    		User.where(role: "guest").each do |guest|
+      			guest.favorite_superlatives.delete_all
+      			Title.where(created_by: guest.id).delete_all
+		    end
+		end
 	end
 
 	def show
 		@user = User.find_by_id(params[:id])
 	end
 
-  # if !(user_signed_in? && current_user.role == "guest")
-  #   User.where(role: "guest").each do |guest|
-  #     guest.favorite_superlatives.delete_all
-  #     Title.where(created_by: guest.id).delete_all
-  #   end
-  # end
+
+
 end
