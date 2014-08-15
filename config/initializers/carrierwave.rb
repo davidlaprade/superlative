ebc698c31fcd6f159e81d5a153ee971d9cec55a6
davidlaprade <david.laprade@gmail.com>
2014-08-15 12:42:18 -0400

    dropped cache directory

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -20,7 +20,7 @@ CarrierWave.configure do |config|
   # end
  
   config.storage = :fog
-  config.cache_dir = "#{Rails.root}/tmp/uploads"                  # To let CarrierWave work on heroku
+  # config.cache_dir = "#{Rails.root}/tmp/uploads"                  # To let CarrierWave work on heroku
  
   config.fog_directory    = ENV['S3_BUCKET_NAME']
   config.fog_public 	  = false
