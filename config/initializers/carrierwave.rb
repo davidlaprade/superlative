dfb8e09167737e804822ca41360adeb8144757d9
davidlaprade <david.laprade@gmail.com>
2014-08-15 10:45:43 -0400

    trying to hook up Carrierwave to AWS S3 bucket on heroku

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,27 +1,27 @@
-# CarrierWave.configure do |config|
-#   config.fog_credentials = {
-#     # In Heroku, follow http://devcenter.heroku.com/articles/config-vars
-#     # $ heroku config:add S3_KEY=your_s3_access_key S3_SECRET=your_s3_secret S3_REGION=eu-west-1 S3_ASSET_URL=http://assets.example.com/ S3_BUCKET_NAME=s3_bucket/folder
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+    # In Heroku, follow http://devcenter.heroku.com/articles/config-vars
+    # $ heroku config:add S3_KEY=your_s3_access_key S3_SECRET=your_s3_secret S3_REGION=eu-west-1 S3_ASSET_URL=http://assets.example.com/ S3_BUCKET_NAME=s3_bucket/folder
  
-#     # Configuration for Amazon S3
-#     :provider              => 'AWS',
-#     :aws_access_key_id     => S3_KEY,
-#     :aws_secret_access_key => S3_SECRET,
-#     :region                => S3_REGION
-#   }
+    # Configuration for Amazon S3
+    :provider              => 'AWS',
+    :aws_access_key_id     => S3_KEY,
+    :aws_secret_access_key => S3_SECRET,
+    :region                => S3_REGION
+  }
  
-#   # # For testing, upload files to local `tmp` folder.
-#   # if Rails.env.test? || Rails.env.cucumber?
-#   #   config.storage = :file
-#   #   config.enable_processing = false
-#   #   config.root = "#{Rails.root}/tmp"
-#   # else
-#   #   config.storage = :fog
-#   # end
+  # # For testing, upload files to local `tmp` folder.
+  # if Rails.env.test? || Rails.env.cucumber?
+  #   config.storage = :file
+  #   config.enable_processing = false
+  #   config.root = "#{Rails.root}/tmp"
+  # else
+  #   config.storage = :fog
+  # end
  
-#   config.cache_dir = "#{Rails.root}/tmp/uploads"                  # To let CarrierWave work on heroku
+  config.cache_dir = "#{Rails.root}/tmp/uploads"                  # To let CarrierWave work on heroku
  
-#   config.fog_directory    = S3_BUCKET_NAME
-#   config.s3_access_policy = :public_read                          # Generate http:// urls. Defaults to :authenticated_read (https://)
-#   config.fog_host         = "#{S3_ASSET_URL}/#{S3_BUCKET_NAME}"
-# end
+  config.fog_directory    = S3_BUCKET_NAME
+  config.s3_access_policy = :public_read                          # Generate http:// urls. Defaults to :authenticated_read (https://)
+  config.fog_host         = "#{S3_ASSET_URL}/#{S3_BUCKET_NAME}"
+end
