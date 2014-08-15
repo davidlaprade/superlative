7adc2a1d395b7dbf68a59ee0f60ce575deea25e4
davidlaprade <david.laprade@gmail.com>
2014-08-15 10:58:05 -0400

    added ENV[] to environment variables for heroku push

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,9 +5,9 @@ CarrierWave.configure do |config|
  
     # Configuration for Amazon S3
     :provider              => 'AWS',
-    :aws_access_key_id     => S3_KEY,
-    :aws_secret_access_key => S3_SECRET,
-    :region                => S3_REGION
+    :aws_access_key_id     => ENV['S3_KEY'],
+    :aws_secret_access_key => ENV['S3_SECRET'],
+    :region                => ENV['S3_REGION']
   }
  
   # # For testing, upload files to local `tmp` folder.
@@ -21,7 +21,7 @@ CarrierWave.configure do |config|
  
   config.cache_dir = "#{Rails.root}/tmp/uploads"                  # To let CarrierWave work on heroku
  
-  config.fog_directory    = S3_BUCKET_NAME
+  config.fog_directory    = ENV['S3_BUCKET_NAME']
   config.s3_access_policy = :public_read                          # Generate http:// urls. Defaults to :authenticated_read (https://)
-  config.fog_host         = "#{S3_ASSET_URL}/#{S3_BUCKET_NAME}"
+  config.fog_host         = "#{ENV['S3_ASSET_URL']}'/#{ENV['S3_BUCKET_NAME']}"
 end
