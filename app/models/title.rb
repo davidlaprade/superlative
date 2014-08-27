08ac8311efceebbe6a02438f621363898c5a2b3a
davidlaprade <david.laprade@gmail.com>
2014-08-27 17:46:47 -0400

    turned off carrierwave config to edit locally, updated styling with bootstrap

--- app/models/title.rb
+++ app/models/title.rb
@@ -5,6 +5,7 @@ has_many :votes, through: :favorites, source: :user
 belongs_to :user
 
 validates_uniqueness_of :content
+validates_presence_of :content, :message => "cannot be blank"
 
 def user_title_was_suggested_for
 	@user = User.find(self.user_id)
