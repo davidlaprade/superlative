1feb4f4cb6f6cfdcd4b070fd31d61a268bf7e628
davidlaprade <david.laprade@gmail.com>
2014-08-31 13:08:27 -0400

    suggest superlatives WAS working, validation error messages just werent displaying, error messages now displaying

--- app/models/title.rb
+++ app/models/title.rb
@@ -4,7 +4,7 @@ has_many :favorites
 has_many :votes, through: :favorites, source: :user
 belongs_to :user
 
-validates_uniqueness_of :content
+validates_uniqueness_of :content, :message => "must be unique"
 validates_presence_of :content, :message => "cannot be blank"
 
 def user_title_was_suggested_for
