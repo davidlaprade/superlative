40c626b6cd30e0fda93480b88d3e13dfc96d5a80
davidlaprade <david.laprade@gmail.com>
2014-10-12 12:32:13 -0400

    added user sign in to navbar in application layout

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,17 @@
 module UsersHelper
+
+	# need this in order to display the sign-in form for devise in the index
+	# https://github.com/plataformatec/devise/wiki/How-To:-Display-a-custom-sign_in-form-anywhere-in-your-app
+	  def resource_name
+	    :user
+	  end
+	 
+	  def resource
+	    @resource ||= User.new
+	  end
+	 
+	  def devise_mapping
+	    @devise_mapping ||= Devise.mappings[:user]
+	  end
+
 end
